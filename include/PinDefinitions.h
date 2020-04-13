63bd4652fb3b3327f3f534bef405b952a4aad964
Gian Claudio Köppel <gckoeppel@gmail.com>
2020-04-13 22:06:48 +0200

    Pin Setup of MCP and Feather GPIOs

---
+++ include/PinDefinitions.h
@@ -0,0 +1,33 @@
+// Defines the pinout of the boards
+
+// Feather Pinout  
+#define LED_GREEN_LEFT 0
+
+#define SWITCH_RED 11
+#define SWITCH_GREEN 12
+#define LED_YELLOW_TOP 13
+
+#define SLIDER_LEFT A0
+#define SLIDER_RIGHT A1
+#define LED_YELLOW_BOTTOM A3
+#define LED_GREEN_RIGHT A4
+#define LED_YELLOW A5
+
+// MCP Pinout
+#define LED_BUTTON_GREEN 0
+#define LED_BUTTON_YELLOW 1
+#define LED_BUTTON_WHITE 2
+#define LED_BUTTON_BLUE 3
+#define LED_BUTTON_RED 4
+#define LED_ARCADE_RED 5
+#define LED_ARCADE_WHITE 6
+#define LED_SWITCH_GREEN 7
+
+#define BUTTON_GREEN 8
+#define BUTTON_YELLOW 9
+#define BUTTON_WHITE 10
+#define BUTTON_BLUE 11
+#define BUTTON_RED 12
+#define BUTTON_ARCADE_RED 13
+#define BUTTON_ARCADE_WHITE 14
+#define LED_SWITCH_RED 15
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,99 @@
 #include <Arduino.h>
+#include "Adafruit_MCP23017.h"
+
+#include "PinDefinitions.h"
+
+Adafruit_MCP23017 mcp;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Setup Running");
+
+  // setup of feather pins
+  pinMode(LED_GREEN_LEFT, OUTPUT); 
+  
+  pinMode(SWITCH_RED, INPUT_PULLUP);
+  pinMode(SWITCH_GREEN, INPUT_PULLUP);
+  pinMode(LED_YELLOW_TOP, OUTPUT);
+
+  pinMode(LED_YELLOW_BOTTOM, OUTPUT);
+  pinMode(LED_GREEN_RIGHT, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  
+  // setup of mcp gpio expander board
+  mcp.begin();                // use default address 0
+  mcp.pinMode(LED_BUTTON_GREEN, OUTPUT);
+  mcp.pinMode(LED_BUTTON_YELLOW, OUTPUT);
+  mcp.pinMode(LED_BUTTON_WHITE, OUTPUT);
+  mcp.pinMode(LED_BUTTON_BLUE, OUTPUT);
+  mcp.pinMode(LED_BUTTON_RED, OUTPUT);
+  mcp.pinMode(LED_ARCADE_RED, OUTPUT);
+  mcp.pinMode(LED_ARCADE_WHITE, OUTPUT);
+  mcp.pinMode(LED_SWITCH_GREEN, OUTPUT);
+  
+  mcp.pinMode(BUTTON_GREEN, INPUT);
+  mcp.pullUp(BUTTON_GREEN, HIGH);
+  mcp.pinMode(BUTTON_YELLOW, INPUT);
+  mcp.pullUp(BUTTON_YELLOW, HIGH);
+  mcp.pinMode(BUTTON_WHITE, INPUT);
+  mcp.pullUp(BUTTON_WHITE, HIGH);
+  mcp.pinMode(BUTTON_BLUE, INPUT);
+  mcp.pullUp(BUTTON_BLUE, HIGH);
+  mcp.pinMode(BUTTON_RED, INPUT);
+  mcp.pullUp(BUTTON_RED, HIGH);
+  mcp.pinMode(BUTTON_ARCADE_RED, INPUT);
+  mcp.pullUp(BUTTON_ARCADE_RED, HIGH);
+  mcp.pinMode(BUTTON_ARCADE_WHITE, INPUT);
+  mcp.pullUp(BUTTON_ARCADE_WHITE, HIGH);
+  mcp.pinMode(LED_SWITCH_RED, OUTPUT);
+
+  int i;
+  for(i=0;i<8;i++)
+  {
+    mcp.digitalWrite(i,HIGH);
+    delay(200);
+    mcp.digitalWrite(i,LOW);
+  }
+  mcp.digitalWrite(15,HIGH);
+  delay(200);
+  mcp.digitalWrite(15,LOW);
+
+  Serial.println("Setup Completed");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // get all input data
+  int sliderLeft = analogRead(SLIDER_LEFT);
+  int sliderRight = analogRead(SLIDER_RIGHT);
+
+  uint8_t buttons = mcp.readGPIO(1); // Read GPIO B
+  buttons &= 0x7F; // mask out top bit, it is an output
+
+  uint8_t switchGreen = digitalRead(SWITCH_GREEN);
+  uint8_t switchRed = digitalRead(SWITCH_RED);
+  
+  Serial.println("==============================================");
+  Serial.println("Current input values:");
+  Serial.print("Slider Left:  ");
+  Serial.print(sliderLeft);
+  Serial.println();
+  Serial.print("Slider Right: ");
+  Serial.print(sliderRight);
+  Serial.println();
+  Serial.print("Buttons:      ");
+  Serial.print(buttons, BIN);
+  Serial.println();
+  Serial.print("Switch Green: ");
+  Serial.print(switchGreen);
+  Serial.println();
+  Serial.print("Switch Red:   ");
+  Serial.print(switchRed);
+  Serial.println();
+  
+
+
+  delay(500);
 }
